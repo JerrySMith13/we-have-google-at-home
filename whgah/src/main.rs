7ed05ef38c89ae8eab638dcc796d9b8b974bb0e0
Jeremy  Smith <jeremy.ws.2008@gmail.com>
2025-09-16 12:29:56 -0500

    Version does not work, but making strides in the logic

--- whgah/src/main.rs
+++ whgah/src/main.rs
@@ -2,5 +2,14 @@ mod site_indexing;
 mod log;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    let url = &args[1];
+
+    let all_links = site_indexing::index_site(url);
+    let links_read = all_links.read().unwrap();
+    for link in links_read.iter() {
+        println!("{link}");
+    }
+
 }
--- whgah/src/site_indexing.rs
+++ whgah/src/site_indexing.rs
@@ -1,38 +1,73 @@
-use std::collections::HashSet;
-use html_parser;
-
+use std::{collections::HashSet, sync::{RwLock, Arc}};
+use html_parser::{self, Node};
+use std::thread;
 use crate::log;
 
 
-pub fn index(url: &str, pre_alloc: usize) -> Vec<&str>{
-    let index_begin: HashSet<&str> = HashSet::with_capacity(pre_alloc);
+fn get_indexes(found: Arc<RwLock<HashSet<String>>>, link: String, root: String) -> Option<()>{
     
+    let page = get_page(link.as_str())?;
+    let page = html_parser::Dom::parse(&page);
 
-    
+    if page.is_err(){
+        return None;
+    }
+    let page = page.unwrap();
+    for child in page.children{
+        get_all_from_node(&child, found.clone(), root.clone());
+    }
+
+    Some(())
 }
 
-fn get_all_indexes(url: &str) -> Option<Vec<&str>>{
-    let webpage: &str = match get_page(url){
-        Some(o) => o,
-        None => return None
-    };
-    //Get all of the urls
-    let page = match html_parser::Dom::parse(webpage){
-        Ok(page) => page,
-        Err(_) => {
-            todo!();  
-            return None;
+fn get_all_from_node(root: &Node, found: Arc<RwLock<HashSet<String>>>, begins_with: String){
+    
+    if let Some(element) = root.element(){
+        let mut own_links = Vec::with_capacity(element.children.len());
+        if element.name.to_lowercase() == "a"{
+            if let Some(href) = element.attributes.get("href"){
+                    if let Some(href) = href{
+                        if href.starts_with(begins_with.as_str()){
+                            if !found.read().unwrap().contains(href){
+                                own_links.push(href.clone());
+                                let links_ref = found.clone();
+                                let href = href.clone();
+                                let root = begins_with;
+                                thread::spawn(move || get_indexes(links_ref, href, root));
+                                todo!("Add thread pool to check state of all threads");
+                            }
+                        }
+                    }
+                }
         }
-    };
-
-    todo!("Add ability to pipe parsed html data into ")
-
-
-    //Pass each to this same function recursively
-
-    return Some(Vec::new())
+        if element.children.len() > 0{
+            for i in 0..element.children.len(){
+                get_all_from_node(&element.children[i], found.clone(), begins_with.clone());
+            }
+        }
+        found.clear_poison();
+        let mut write = found.write().unwrap();
+        for link in own_links{
+            write.insert(link);
+        }
+    }
+    
 }
 
-fn get_page(url: &str) -> Option<&str>{
-    todo!()
+fn get_page(url: &str) -> Option<String> {
+    let response = reqwest::blocking::get(url).ok()?;
+    if response.status() != reqwest::StatusCode::OK {
+        return None;
+    }
+    response.text().ok()
+}
+
+pub fn index_site(url: &str) -> Arc<RwLock<HashSet<String>>>{
+    let all_links: Arc<RwLock<HashSet<String>>> = Arc::new(
+        RwLock::new(HashSet::with_capacity(100))
+    );
+
+    let root = url.split_once('/').unwrap_or((url, "")).0;
+    get_indexes(all_links.clone(), url.to_string(), root.to_string());
+    return all_links;
 }
